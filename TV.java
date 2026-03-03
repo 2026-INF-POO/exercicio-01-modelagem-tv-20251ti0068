676be3f175f11fd17e2fe2135453126206b27ec4
20251ti0068 <20251ti0068@ifba.edu.br>
2026-03-03 09:05:33 -0300

    Update TV.java

--- TV.java
+++ TV.java
@@ -1,1 +1,27 @@
+class TV{
+  int polegadas=45;
+  int volume;
+  int canal;
+  int voltagem=110;
+  String marca;
 
+  void ligar (){
+   int consumo= voltagem * polegadas
+    printf ("O consumo é igual a %d%n", consumo);
+  }
+ int aumentarVolume (){
+    if (volume<10){
+      volume++;
+    return volume; 
+    }
+
+   void diminuirVolume
+    
+  
+}
+
+//A televisão pode realizar as seguintes ações:
+
+desligar;
+aumentar e diminuir o volume;
+subir e descer canal.
